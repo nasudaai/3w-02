3444e21ffe8dce2a96b62b09eecee9971a94b40b
nasuda <nasuda908@gmail.com>
2026-03-17 15:58:13 +0900

    update compose

--- second-week/compose/main.mjs
+++ second-week/compose/main.mjs
@@ -1,4 +1,8 @@
 const K = process.argv[2];
+
+const cMajorScale = ["c", "d", "e", "f", "g", "a", "b"];
+console.log(cMajorScale, cMajorScale.length);
+
 console.log(K);
 let key = "C";
 console.log(key);
