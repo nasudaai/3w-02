455d15f826de8866329b084e2c9f7ac8a9e5af01
nasuda <nasuda908@gmail.com>
2026-03-24 01:34:30 +0900

    add eary return

--- third-week/script.mjs
+++ third-week/script.mjs
@@ -1,7 +1,10 @@
 const args = process.argv.slice(2);
 process.stdout.write(JSON.stringify(args));
 
+if (args.length === 0) {
+  process.stdout.write("pleas argv");
+  process.exit(0);
+}
+
 const json = JSON.stringify(args);
 console.log(json);
-
-
