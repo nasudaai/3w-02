507f59ef6098ed685edc271102150ca57bace158
nasuda <nasuda908@gmail.com>
2026-03-24 09:28:38 +0900

    add obj

--- third-week/script.mjs
+++ third-week/script.mjs
@@ -1,8 +1,14 @@
 const args = process.argv.slice(2);
-process.stdout.write(JSON.stringify(args));
+process.stdout.write(JSON.stringify(args) + "\n");
+
+const obj = {
+  key: "value",
+};
 
 if (args.length === 0) {
-  process.stdout.write("pleas argv");
+  process.stdout.write("pleas argv\n");
+
+  console.log(`default ${obj.key}`)
   process.exit(0);
 }
 
