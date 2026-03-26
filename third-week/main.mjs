f920f4519f5ed515a6adae2c39c2af8274a804ac
nasuda <nasuda908@gmail.com>
2026-03-27 07:30:57 +0900

    add main

--- third-week/main.mjs
+++ third-week/main.mjs
@@ -3,9 +3,14 @@ const args = process.argv.slice(2);
 
 const json = JSON.stringify(args);
 
+const obj = {};
+
 function main() {
   console.log("test");
   process.stdout.write(json + "\n");
+
+  obj.one = args[0];
+  console.log(obj);
 };
 
 main();
