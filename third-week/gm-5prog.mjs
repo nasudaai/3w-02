440633cedda763d22e5303cd36edfefd52d74e4f
nasuda <nasuda908@gmail.com>
2026-03-28 12:59:10 +0900

    add mode

--- third-week/gm-5prog.mjs
+++ third-week/gm-5prog.mjs
@@ -1,8 +1,12 @@
-//const args = process.argv.slice(2);
+const args = process.argv.slice(2);
+const mode = args[0];
+console.log(mode);
 
 const key = "Gm";
-const cannonProg  = ["Gm", "Dm", "Eflat", "Bflat", "Cm", "Gm", "Cm Eflat Am7-5", "D"];
-console.log(arr);
 
-const fourValiation = arr[7-1];
+//fiveProg
+const cannonProg  = ["Gm", "Dm", "Eflat", "Bflat", "Cm", "Gm", "Cm Eflat Am7-5", "D"];
+console.log(cannonProg);
+
+const fourValiation = cannonProg[7-1];
 console.log(fourValiation);
